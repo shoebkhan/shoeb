69667af1e085581d98938de075d5f4b1b527e231
Shoeb <shoeb.niet@gmail.com>
2025-04-15 14:09:55 +0530

    commit

---
+++ EmmasShoeb/EmmasShoeb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EmmasShoeb
+//
+//  Created by IDS Logic on 15/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EmmasShoeb/EmmasShoebTests/EmmasShoebTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmmasShoebTests.swift
+//  EmmasShoebTests
+//
+//  Created by IDS Logic on 15/04/25.
+//
+
+import Testing
+@testable import EmmasShoeb
+
+struct EmmasShoebTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
